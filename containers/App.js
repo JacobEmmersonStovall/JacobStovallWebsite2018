dd3013cd143a65b90d0fa16e6ab398d372cc4b02
Jacob Stovall <jacob.stovall@asu.edu>
2018-01-01 22:03:30 -0700

    Brought 2018 site up to where 2017 was.

--- containers/App.js
+++ containers/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AnchorTagListItem from './AnchorTagListItem';
+import '../styles/general.less';
 import '../styles/app.less';
 
 class App extends React.Component {
   render(){
     return (
       <div className="appDiv">
-        <p>This is my new website I will build overtime.</p>
+        <img src="../images/Jacob_Vector.png" />
+        <p>Software Developer. Improv Person. Just a Guy.</p>
         <ul>
           <AnchorTagListItem link="mailto:jacobestovall@gmail.com" text="Email Me." />
           <AnchorTagListItem link="https://www.linkedin.com/in/jacob-stovall-14b2a093" text="Look at LinkedIn." />

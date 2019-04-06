ad6fbd758c217947caac81353767501578c64826
Jacob Stovall <jacobstovall@Jacobs-MacBook-Air.local>
2019-04-06 11:58:42 -0700

    Undid build stuff

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
     return (
       <div className="appDiv">
         <img src="./images/Jacob_Vector.png" />
-        <p>Software Developer. Improv Person. Just a Guy.</p>
+        <p>Software Developer. Improv Person. Just a Person.</p>
         <ul>
           <AnchorTagListItem link="mailto:jacobestovall@gmail.com" text="Email Me." />
           <AnchorTagListItem link="https://www.linkedin.com/in/jacob-stovall-14b2a093" text="Look at LinkedIn." />
